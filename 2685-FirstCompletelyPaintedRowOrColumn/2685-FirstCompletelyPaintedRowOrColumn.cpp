b71b6d0be35c28df45de2a3141b96c2e16660812
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:13:10 +0530

    Create 2685-FirstCompletelyPaintedRowOrColumn

---
+++ 2685-FirstCompletelyPaintedRowOrColumn/2685-FirstCompletelyPaintedRowOrColumn.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/19/2026, 10:13:07 PM
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+        unordered_map<int, pair<int, int>> pos;
+        
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++)
+                pos[mat[i][j]] = {i, j};
+        
+        vector<int> rCnt(row, 0);
+        vector<int> cCnt(col, 0);
+        
+        for(int i = 0; i < arr.size(); i++) {
+            auto [r, c] = pos[arr[i]];
+            rCnt[r]++;
+            cCnt[c]++;
+            if(rCnt[r] == col || cCnt[c] == row)
+                return i;
+        }
+        return arr.size() - 1;
+    }
+};
+
+
+        
+
