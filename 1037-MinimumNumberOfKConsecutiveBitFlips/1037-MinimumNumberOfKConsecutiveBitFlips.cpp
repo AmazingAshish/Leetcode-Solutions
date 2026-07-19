7b24419cb1482a1ee2553d0518316d22d8512b51
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:20:07 +0530

    Create 1037-MinimumNumberOfKConsecutiveBitFlips

---
+++ 1037-MinimumNumberOfKConsecutiveBitFlips/1037-MinimumNumberOfKConsecutiveBitFlips.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/19/2026, 10:20:05 PM
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int flipped = 0;
+        int res = 0;
+        vector<int> isFlipped(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (i >= k) {
+                flipped ^= isFlipped[i - k];
+            }
+            if (flipped == nums[i]) {
+                if (i + k > n) {
+                    return -1;
+                }
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
