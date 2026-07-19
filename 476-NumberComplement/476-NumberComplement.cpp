7ec25304eca913f2f836fadac968c27fe2badd3b
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:22:30 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/476-NumberComplement.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/19/2026, 10:22:28 PM
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
+        }
+        
+        return ~mask & ~num;
+    }
+};
