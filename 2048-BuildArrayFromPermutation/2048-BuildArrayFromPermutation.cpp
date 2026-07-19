40e07e850d019a5f6bd53b63c40fa82101c74a7b
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:16:17 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ 2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/19/2026, 10:16:15 PM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans(nums.size()) ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            ans[i] = nums[nums[i]] ;
+        }return ans ;
+    }
+};
