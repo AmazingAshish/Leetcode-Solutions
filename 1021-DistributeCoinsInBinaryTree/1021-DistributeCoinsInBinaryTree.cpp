443403712956c98884bd189a9a02852e160a6b65
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:20:12 +0530

    Create 1021-DistributeCoinsInBinaryTree

---
+++ 1021-DistributeCoinsInBinaryTree/1021-DistributeCoinsInBinaryTree.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/19/2026, 10:20:10 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        static int distributeCoins(TreeNode* root, TreeNode* parent= NULL) {
+        if (!root) return 0;
+        int  moves=distributeCoins(root->left, root)+distributeCoins(root->right, root);
+        int x=root->val-1;// give x coins to parent node
+        if (parent) parent->val += x; // now parent's coins
+        moves+=abs(x);
+        return moves;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
