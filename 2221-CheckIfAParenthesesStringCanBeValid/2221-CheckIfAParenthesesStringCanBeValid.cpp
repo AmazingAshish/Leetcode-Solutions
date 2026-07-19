8d39c7518f91cbd5231be17766ee0b3c2d6936c6
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:15:23 +0530

    Create 2221-CheckIfAParenthesesStringCanBeValid

---
+++ 2221-CheckIfAParenthesesStringCanBeValid/2221-CheckIfAParenthesesStringCanBeValid.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/19/2026, 10:15:21 PM
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false; // Odd length cannot form valid parentheses
+        }
+
+        // Left-to-right pass: Ensure there are enough open brackets
+        int openCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(' || locked[i] == '0') {
+                openCount++;
+            } else { // s[i] == ')' and locked[i] == '1'
+                openCount--;
+            }
+            if (openCount < 0) {
+                return false; // Too many ')' encountered
+            }
+        }
+
+        // Right-to-left pass: Ensure there are enough close brackets
+        int closeCount = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')' || locked[i] == '0') {
+                closeCount++;
+            } else { // s[i] == '(' and locked[i] == '1'
+                closeCount--;
+            }
+            if (closeCount < 0) {
+                return false; // Too many '(' encountered
+            }
+        }
+
+        return true;
+    }
+};
