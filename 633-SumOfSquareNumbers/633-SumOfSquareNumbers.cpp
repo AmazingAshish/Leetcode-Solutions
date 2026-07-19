f1ad088cf77fcb94298d8d8b7edc84462892d422
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:21:49 +0530

    Create 633-SumOfSquareNumbers

---
+++ 633-SumOfSquareNumbers/633-SumOfSquareNumbers.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:21:46 PM
+class Solution {
+public:
+    bool judgeSquareSum(int c) 
+    {
+        long long int i=1,j=sqrt(c);
+        if(j*j==c)
+        return true;
+        while(i<=j)
+        {
+           long long sum = (i*i)+(j*j);
+           if(sum==c)
+           return true;
+           else if(sum<c)
+           i++;
+           else
+           j--;
+        }
+        return false;
+        
+    }
+};
