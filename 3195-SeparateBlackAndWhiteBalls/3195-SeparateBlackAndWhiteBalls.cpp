fa5820f55af32d4f851d8deb8439bd16f158e9e4
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:12:10 +0530

    Create 3195-SeparateBlackAndWhiteBalls

---
+++ 3195-SeparateBlackAndWhiteBalls/3195-SeparateBlackAndWhiteBalls.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/19/2026, 10:12:08 PM
+class Solution {
+public:
+    long long minimumSteps(string& s) {
+        int black=0;
+        long long  swap=0;
+        for(char c: s){
+            (c=='1')?black++:swap+=black;
+        }
+        return swap;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
