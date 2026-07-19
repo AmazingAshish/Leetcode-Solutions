23c6803f7da89956654debefd07e0e893dd13e09
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:12:13 +0530

    Create 3189-FindChampionIi

---
+++ 3189-FindChampionIi/3189-FindChampionIi.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/19/2026, 10:12:11 PM
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> incoming(n, 0);
+        
+        for (const auto& edge : edges) {
+            incoming[edge[1]]++;
+        }
+        
+        vector<int> champions;
+        for (int i = 0; i < n; i++) {
+            if (incoming[i] == 0) {
+                champions.push_back(i);
+            }
+        }
+        
+        if (champions.size() > 1) {
+            return -1;
+        }
+        
+        return champions[0];
+    }
+};
