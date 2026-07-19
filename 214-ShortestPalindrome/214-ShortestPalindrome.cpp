457d6c711d0aa64a3c2c680c06a8fd3df498b93d
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:23:38 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/214-ShortestPalindrome.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/19/2026, 10:23:36 PM
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        const int n = s.size();
+        int i = 0;
+        for (int j=n-1; j>= 0; j--) {
+            while (j>=0 && s[i] == s[j])
+                i++, j--;
+        }
+        if (i==n) //palindrome early stop
+            return s;
+        string sub= s.substr(i), remain_rev=sub;
+        reverse(remain_rev.begin(), remain_rev.end());
+        return remain_rev + shortestPalindrome(s.substr(0, i)) + sub;
+    }
+};
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
