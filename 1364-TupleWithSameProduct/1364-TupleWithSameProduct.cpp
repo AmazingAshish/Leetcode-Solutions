f1deb020bb4e3645b1da5fd9f1ffca9bf50e04e2
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:19:17 +0530

    Create 1364-TupleWithSameProduct

---
+++ 1364-TupleWithSameProduct/1364-TupleWithSameProduct.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/19/2026, 10:19:15 PM
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                ans += 8 * mp[product];
+                mp[product]++;
+            }
+        return ans;
+    }
+};
