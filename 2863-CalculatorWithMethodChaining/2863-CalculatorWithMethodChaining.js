ec75c2e546f0030316f273a02904e89c74ef6777
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:12:35 +0530

    Create 2863-CalculatorWithMethodChaining

---
+++ 2863-CalculatorWithMethodChaining/2863-CalculatorWithMethodChaining.js
@@ -0,0 +1,46 @@
+// Last updated: 7/19/2026, 10:12:33 PM
+class Calculator {
+    // @param {number} value
+    constructor(value) {
+        this.result = value;
+    }
+    
+    //-------- @return {Calculator} -------- STARTS
+    // @param {number} value
+    add(value){
+        this.result += value;
+        return this;
+    }
+    
+    // @param {number} value
+    subtract(value){
+        this.result -= value;
+        return this;
+    }
+    
+    // @param {number} value 
+    multiply(value) {
+        this.result *= value;
+        return this;
+    }
+    
+    // @param {number} value
+    divide(value) {
+        if(value == 0) throw new Error('Division by zero is not allowed');
+        this.result /= value;
+        return this;
+    }
+    
+    // @param {number} value
+    power(value) {
+        this.result = Math.pow(this.result, value);
+        return this;
+    }
+    
+    //-------- @return {Calculator} -------- ENDS
+
+    // @return {number}
+    getResult() {
+        return this.result;
+    }
+}
