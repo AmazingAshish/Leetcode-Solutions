78650a10a7f274a975bd6ef3dd27ac565e0bf9de
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:21:40 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/19/2026, 10:21:37 PM
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+};
