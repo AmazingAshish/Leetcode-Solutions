f7cf830d1cd0294a3a29226792e397cc72ee1631
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:23:30 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/19/2026, 10:23:28 PM
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> listVals;
+        while (head) {
+            listVals.push_back(head->val);
+            head = head->next;
+        }
+        
+        int left = 0, right = listVals.size() - 1;
+        while (left < right && listVals[left] == listVals[right]) {
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+};
