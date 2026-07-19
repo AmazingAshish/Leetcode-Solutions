fc6620de4b43db6aa42ca0922b57754de907b2bc
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:20:46 +0530

    Create 900-ReorderedPowerOf2

---
+++ 900-ReorderedPowerOf2/900-ReorderedPowerOf2.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/19/2026, 10:20:44 PM
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        auto countDigits = [](int x) {
+            string s = to_string(x);
+            sort(s.begin(), s.end());
+            return s;
+        };
+        string target = countDigits(n);
+        for (int i = 0; i < 31; i++) {
+            if (countDigits(1 << i) == target) return true;
+        }
+        return false;
+    }
+};
