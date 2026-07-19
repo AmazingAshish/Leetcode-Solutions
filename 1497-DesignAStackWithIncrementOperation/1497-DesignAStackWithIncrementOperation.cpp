77f443d717581bcb0055e2e057c83563156e1eea
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:18:37 +0530

    Create 1497-DesignAStackWithIncrementOperation

---
+++ 1497-DesignAStackWithIncrementOperation/1497-DesignAStackWithIncrementOperation.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/19/2026, 10:18:35 PM
+class CustomStack {
+public:
+    vector<int> stack;
+    int top;
+
+    CustomStack(int maxSize) {
+        stack.resize(maxSize);
+        top = -1;
+    }
+
+   void push(int x) {
+if (top== -1 || top < stack.size() - 1) {
+top++;
+stack[top] = x;
+}
+}
+
+    int pop() {
+        if (top != -1) {
+            return stack[top--];
+        }
+        return -1;
+    }
+
+    void increment(int k, int val) {
+        for (int i = 0; i < min(k, top + 1); i++) {
+            stack[i] += val;
+        }
+    }
+};
