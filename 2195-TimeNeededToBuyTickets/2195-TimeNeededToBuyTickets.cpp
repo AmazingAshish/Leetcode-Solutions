6be32afe42a800bac86aee4490211135bd2ad036
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:15:33 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/19/2026, 10:15:31 PM
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& ts, int k) {
+        int res = 0, target = ts[k];
+        // parsing the queue up to k, included
+        for (int i = 0; i <= k; i++) {
+            // getting the minimum between the current value and target
+            res += min(ts[i], target);
+        }
+        // parsing the queue up from  k, excluded
+        target--;
+        for (int i = k + 1, lmt = ts.size(); i < lmt; i++) {
+            // getting the minimum between the current value and target
+            res += min(ts[i], target);
+        }
+        return res;
+    }
+};
