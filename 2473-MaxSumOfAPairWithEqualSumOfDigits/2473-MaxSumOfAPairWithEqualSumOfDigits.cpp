5ed2b61e8ae63ba1f7c4891025249b718f892aae
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:14:10 +0530

    Create 2473-MaxSumOfAPairWithEqualSumOfDigits

---
+++ 2473-MaxSumOfAPairWithEqualSumOfDigits/2473-MaxSumOfAPairWithEqualSumOfDigits.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/19/2026, 10:14:07 PM
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        int max[82] = {0};
+        int ans = -1;
+        for (int x : nums) {
+            int sum = 0, temp = x;
+            while (temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (max[sum] != 0) ans = std::max(ans, x + max[sum]);
+            max[sum] = std::max(max[sum], x);
+        }
+        return ans;
+    }
+};
