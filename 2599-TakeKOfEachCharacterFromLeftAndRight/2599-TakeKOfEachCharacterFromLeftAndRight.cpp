ed3d3ed81bcadafbbfe31bfec4799edc2ce4737e
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:13:26 +0530

    Create 2599-TakeKOfEachCharacterFromLeftAndRight

---
+++ 2599-TakeKOfEachCharacterFromLeftAndRight/2599-TakeKOfEachCharacterFromLeftAndRight.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/19/2026, 10:13:24 PM
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        // Total counts
+        vector<int> count(3, 0);
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        
+        if (*min_element(count.begin(), count.end()) < k) {
+            return -1;
+        }
+        
+        // Sliding Window
+        int res = INT_MAX;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count[s[r] - 'a']--;
+            
+            while (*min_element(count.begin(), count.end()) < k) {
+                count[s[l] - 'a']++;
+                l++;
+            }
+            res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+        }
+        return res;
+    }
+};
