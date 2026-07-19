3afe1628d53f2599ee5a781a17d7ee62d6bb66e1
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:24:57 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/19/2026, 10:24:55 PM
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int i = 0, left_max = height[0], sum = 0;
+        int j = height.size() - 1, right_max = height[j];
+        while (i < j) {
+            if (left_max <= right_max) {
+                sum += (left_max - height[i]);
+                i++;
+                left_max = std::max(left_max, height[i]);
+            } else {
+                sum += (right_max - height[j]);
+                j--;
+                right_max = std::max(right_max, height[j]);
+            }
+        }
+        return sum;
+    }
+};
