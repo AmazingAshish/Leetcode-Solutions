2e4e67bb6f8a7c38d4499900e9dc707244c0693a
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:15:36 +0530

    Create 2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints

---
+++ 2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints/2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/19/2026, 10:15:34 PM
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        ListNode* pre = head; 
+        ListNode*cur = head -> next;
+        vector<int> ans = {-1, -1};
+        int prePosition = -1, curPosition = -1, firstPosition = -1, position = 0;
+        while(cur -> next != NULL) {
+            if((cur -> val < pre -> val && cur -> val < cur -> next -> val) || (cur -> val > pre -> val && cur -> val > cur -> next -> val)) {
+                //local
+                prePosition = curPosition;
+                curPosition = position;
+                if(firstPosition == -1) {
+                    firstPosition = position;
+                }
+                if(prePosition != -1) {
+                    if(ans[0] == -1) ans[0] = curPosition - prePosition;
+                    else ans[0] = min(ans[0], curPosition - prePosition);
+                    ans[1] = position - firstPosition;
+                }
+            }
+            position++;
+            pre = cur;
+            cur = cur -> next;
+        }
+        return ans;
+    }
+};
