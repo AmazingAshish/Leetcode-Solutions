af01c0abf1c8cb29c6ac06a0c4adbb7655415ea1
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:16:06 +0530

    Create 2076-SumOfDigitsOfStringAfterConvert

---
+++ 2076-SumOfDigitsOfStringAfterConvert/2076-SumOfDigitsOfStringAfterConvert.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:16:03 PM
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        // Convert each character in the string to its corresponding numeric value
+        string number = "";
+        for (char x : s) {
+            number += to_string(x - 'a' + 1);
+        }
+        
+        // Perform the transformation `k` times
+        while (k > 0) {
+            int temp = 0;
+            for (char x : number) {
+                temp += x - '0';  // Sum the digits of the current number
+            }
+            number = to_string(temp);  // Convert the sum back to a string
+            k--;
+        }
+        return stoi(number);  // Return the final result as an integer
+    }
+};
