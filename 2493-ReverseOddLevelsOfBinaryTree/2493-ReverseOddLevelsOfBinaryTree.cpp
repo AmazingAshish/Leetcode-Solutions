09949092664ac0a679a1bdd07b0511da7be4b004
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:14:04 +0530

    Create 2493-ReverseOddLevelsOfBinaryTree

---
+++ 2493-ReverseOddLevelsOfBinaryTree/2493-ReverseOddLevelsOfBinaryTree.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:14:02 PM
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        DFS(root->left, root->right, 0);
+        return root;
+    }
+
+private:
+    void DFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+        if (leftChild == nullptr || rightChild == nullptr) {
+            return;
+        }
+        if (level % 2 == 0) {
+            int temp = leftChild->val;
+            leftChild->val = rightChild->val;
+            rightChild->val = temp;
+        }
+        DFS(leftChild->left, rightChild->right, level + 1);
+        DFS(leftChild->right, rightChild->left, level + 1);
+    }
+};
