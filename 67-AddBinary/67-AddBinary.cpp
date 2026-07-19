206097be63297d2c72722179f1187e9c828efecc
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:24:43 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:24:41 PM
+class Solution {
+ public:
+  string addBinary(string a, string b) {
+    string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};
