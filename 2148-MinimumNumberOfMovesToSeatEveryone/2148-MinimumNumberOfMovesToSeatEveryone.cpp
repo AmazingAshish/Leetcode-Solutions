682e7e595c675bffea47d02718499f29f5c12ab0
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:15:48 +0530

    Create 2148-MinimumNumberOfMovesToSeatEveryone

---
+++ 2148-MinimumNumberOfMovesToSeatEveryone/2148-MinimumNumberOfMovesToSeatEveryone.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/19/2026, 10:15:46 PM
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+       sort(seats.begin(), seats.end());
+       sort(students.begin(), students.end());
+        int moves = 0;
+        for( int i=0; i<seats.size(); i++)
+            moves += abs(seats[i] - students[i]);
+        
+        return moves;
+    }
+};
