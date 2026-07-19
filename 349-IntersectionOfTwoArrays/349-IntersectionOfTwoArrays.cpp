00774386bc64d041cd4bfeff406727575ff28a0c
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:22:57 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/19/2026, 10:22:55 PM
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>temp;
+        for(int i=0; i<nums1.size(); i++){
+            for(int j=0; j<nums2.size();j++){
+                if(nums1[i]==nums2[j]) temp.insert(nums1[i]);
+            }
+        }
+        vector<int>ans(temp.begin(),temp.end());
+        return ans;
+    }
+};
