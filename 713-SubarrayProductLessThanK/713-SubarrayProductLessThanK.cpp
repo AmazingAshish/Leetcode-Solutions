12122fa544c044c5ccfaad5f533c621c0789d892
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:21:34 +0530

    Create 713-SubarrayProductLessThanK

---
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/19/2026, 10:21:32 PM
+class Solution {
+public:
+  int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+    int left = 0, right = 0, product = 1, count = 0;
+    int n = nums.size();
+    if(k <= 1) return 0;
+    while (right < n) {
+      product *= nums[right];
+      while (product >= k) product /= nums[left++];
+      count += 1 + (right - left);
+      right++;
+    }
+    return count;
+  }
+};
