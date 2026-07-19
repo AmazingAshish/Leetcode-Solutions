88745e1984ddd56797471e971d853e4916494787
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:20:22 +0530

    Create 987-RevealCardsInIncreasingOrder

---
+++ 987-RevealCardsInIncreasingOrder/987-RevealCardsInIncreasingOrder.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:20:20 PM
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n = deck.size();
+        sort(deck.begin(), deck.end());
+        queue<int> q; // o to n-1
+        for(int i = 0; i < n; i++) {
+            q.push(i);
+        }
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++) {
+            int idx = q.front();
+            q.pop();
+            // popping front and pushing it to back
+            q.push(q.front());
+            q.pop();
+            ans[idx] = deck[i];
+        }
+        return ans;
+    }
+};
