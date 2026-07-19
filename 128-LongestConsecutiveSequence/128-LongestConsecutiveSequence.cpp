6d2c9be514dcc7ba9b7ae39a94308304791086d1
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:24:17 +0530

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/19/2026, 10:24:15 PM
+class Solution {
+public:
+    int longestConsecutive(std::vector<int>& nums) {
+        std::unordered_map<int, std::pair<int, int>> mp;
+        std::unordered_map<int, bool> bl;
+        int mx = 0;
+        
+        for (int i : nums) {
+            if (bl[i]) {
+                continue;
+            }
+            bl[i] = true;
+            int l = i, r = i;
+            
+            if (mp.find(i + 1) != mp.end()) {
+                r = mp[i + 1].first;
+            }
+            if (mp.find(i - 1) != mp.end()) {
+                l = mp[i - 1].second;
+            }
+            mp[r] = std::make_pair(r, l);
+            mp[l] = std::make_pair(r, l);
+            mx = std::max(mx, r - l + 1);
+        }
+        
+        return mx;
+    }
+};
