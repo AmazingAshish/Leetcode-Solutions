69c761bf8a8ff884615b9281fa2a4f9b7d58d3a7
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:12:42 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,11 @@
+// Last updated: 7/19/2026, 10:12:40 PM
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
