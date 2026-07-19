ee8d9f6b2e797a1820735057d01699d350569cd4
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:16:18 +0530

    Create 2044-NumberOfWonderfulSubstrings

---
+++ 2044-NumberOfWonderfulSubstrings/2044-NumberOfWonderfulSubstrings.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:16:16 PM
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        vector<long long> count(1024, 0); // 2^10 to store XOR values
+        long long result = 0;
+        int prefixXor = 0;
+        count[prefixXor] = 1;
+
+        for (char ch : word) {
+            int charIndex = ch - 'a';
+            prefixXor ^= 1 << charIndex;
+            result += count[prefixXor];
+            for (int i = 0; i < 10; i++) {
+                result += count[prefixXor ^ (1 << i)];
+            }
+            count[prefixXor]++;
+        }
+
+        return result;
+    }
+};
