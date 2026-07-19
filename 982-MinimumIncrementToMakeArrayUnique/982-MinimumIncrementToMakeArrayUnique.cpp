8296cdc747eaa2e9099d9e91b67e78fcf68c6c0f
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:20:27 +0530

    Create 982-MinimumIncrementToMakeArrayUnique

---
+++ 982-MinimumIncrementToMakeArrayUnique/982-MinimumIncrementToMakeArrayUnique.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/19/2026, 10:20:24 PM
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        // Sort the vector first
+        std::sort(nums.begin(), nums.end());
+
+        int numTracker = 0; // Tracks the next unique number that should be set.
+        int minIncrement = 0; // Counts the total increments required.
+
+        for (int num : nums) {
+            numTracker = std::max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1; // Increment the tracker for the next number.
+        }
+
+        return minIncrement;
+    }
+};
