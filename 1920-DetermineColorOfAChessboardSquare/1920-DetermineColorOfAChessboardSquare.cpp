dc63a38b5b58bf738bcd746a5482f359dcd83b0c
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:16:42 +0530

    Create 1920-DetermineColorOfAChessboardSquare

---
+++ 1920-DetermineColorOfAChessboardSquare/1920-DetermineColorOfAChessboardSquare.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/19/2026, 10:16:40 PM
+class Solution {
+public:
+    bool squareIsWhite(string c) {
+        if(c=="a2"||c=="a4"||c=="a6"||c=="a8"||c=="b1"||c=="b3"||c=="b5"||c=="b7"||c=="c2"||c=="c4"||c=="c6"||c=="c8"||c=="d1"||c=="d3"||c=="d5"||c=="d7"||c=="e2"||c=="e4"||c=="e6"||c=="e8"||c=="f1"||c=="f3"||c=="f5"||c=="f7"||c=="g2"||c=="g4"||c=="g6"||c=="g8"||c=="h1"||c=="h3"||c=="h5"||c=="h7"){
+            return true;
+        }return false;
+    }
+};
