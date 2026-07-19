66dc7334e89355f9303b6686695828c8c865b000
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:14:59 +0530

    Create 2316-CountHillsAndValleysInAnArray

---
+++ 2316-CountHillsAndValleysInAnArray/2316-CountHillsAndValleysInAnArray.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/19/2026, 10:14:57 PM
+class Solution {
+public:
+    int countHillValley(std::vector<int>& nums) {
+        int count = 0;
+        int left = 0;
+
+        for (int i = 1; i < nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    ++count;
+                }
+                left = i;
+            }
+        }
+
+        return count;
+    }
+};
