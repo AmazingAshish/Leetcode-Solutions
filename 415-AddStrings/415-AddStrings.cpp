1c8868ff23baba6f34fdce8d892c2e30e4d248d0
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:22:40 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/19/2026, 10:22:38 PM
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        auto output = ""s;
+        int i = num1.size() - 1, j = num2.size() - 1, sum = 0;
+        while (i >= 0 || j >= 0 || sum) {
+            if (i >= 0) 
+                sum += num1[i--] - '0';
+            if (j >= 0) 
+                sum += num2[j--] - '0';
+            output += (sum % 10) + '0';
+            sum /= 10;
+        }
+        reverse(begin(output), end(output));
+        return output;
+    }
+};
