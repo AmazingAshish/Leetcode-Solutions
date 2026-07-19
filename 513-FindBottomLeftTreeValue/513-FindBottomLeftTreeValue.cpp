426669dbc87ddeed928a258b93062fe5a49aba5f
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:22:19 +0530

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/19/2026, 10:22:17 PM
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int leftmost_value;
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            leftmost_value = node->val;
+
+            if (node->right) {
+                q.push(node->right);
+            }
+            if (node->left) {
+                q.push(node->left);
+            }
+        }
+
+        return leftmost_value;
+    }
+};
