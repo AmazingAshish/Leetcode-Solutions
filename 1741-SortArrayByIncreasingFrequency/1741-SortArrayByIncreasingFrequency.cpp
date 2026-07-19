1546b786995e357e82ffe1642f2bfd56f4903082
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:17:32 +0530

    Create 1741-SortArrayByIncreasingFrequency

---
+++ 1741-SortArrayByIncreasingFrequency/1741-SortArrayByIncreasingFrequency.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/19/2026, 10:17:30 PM
+class Solution {
+public:
+    using int2=array<int,2>;
+    vector<int> frequencySort(vector<int>& nums) {
+        int n=nums.size(), freq[201]={0};
+        for(int x:nums){
+            freq[x+100]++;
+        }
+        
+        sort(nums.begin(), nums.end(), [&](int x, int y){
+            return (freq[x+100]==freq[y+100])?x>y:freq[x+100]<freq[y+100];
+        });
+        return nums;
+    }
+};
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
