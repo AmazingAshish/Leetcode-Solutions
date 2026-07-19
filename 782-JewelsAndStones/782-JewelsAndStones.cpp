36b399baadd3133b4358532e13b524519d9bdcf9
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:21:17 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/19/2026, 10:21:15 PM
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int hash[256] = {0};
+        int count = 0;
+        for(int i = 0;i<stones.size();i++) {
+            hash[stones[i]]++;
+        }
+        for(int i = 0;i<jewels.size();i++) {
+            count = count + hash[jewels[i]];
+        }
+        return count;
+    }
+};
