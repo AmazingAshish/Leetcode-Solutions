61f6e097503273006a86f6a0c2531924b5dbecbe
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:16:53 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/19/2026, 10:16:51 PM
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string mergedString = "";
+        int i = 0, j = 0;
+        while (i < word1.size() && j < word2.size()) {
+            mergedString += word1[i++];
+            mergedString += word2[j++];
+        }
+        mergedString += word1.substr(i);
+        mergedString += word2.substr(j);
+        return mergedString;
+    }
+};
