346f88faa9578363f94c069e8e735d6aa1435d80
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:18:24 +0530

    Create 1537-MaximumScoreAfterSplittingAString

---
+++ 1537-MaximumScoreAfterSplittingAString/1537-MaximumScoreAfterSplittingAString.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/19/2026, 10:18:21 PM
+class Solution {
+public:
+    int maxScore(string s) {
+        int Total_Zeros=0;
+        for(char c:s){
+            if(c=='0')Total_Zeros++;
+        }
+        int ans=-1;
+        int zeros=0;
+        int L=s.length();
+        for(int i=1;i<L;i++){
+            if(s.at(i-1)=='0'){
+                zeros++;
+                Total_Zeros--;
+            }
+            ans=max(ans,(zeros+(L-Total_Zeros-(i))));
+        }
+        return ans;
+        
+    }
+};
