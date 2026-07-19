8727e04322ccbf297806cfd097018421a277ab18
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:15:38 +0530

    Create 2179-MostBeautifulItemForEachQuery

---
+++ 2179-MostBeautifulItemForEachQuery/2179-MostBeautifulItemForEachQuery.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/19/2026, 10:15:35 PM
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end());
+        vector<pair<int, int>> queriesWithIndex;
+        for (int i = 0; i < queries.size(); i++) {
+            queriesWithIndex.push_back({queries[i], i});
+        }
+        sort(queriesWithIndex.begin(), queriesWithIndex.end());
+        
+        vector<int> res(queries.size());
+        int maxBea = 0;
+        int j = 0;
+        for (auto [q, i] : queriesWithIndex) {
+            while (j < items.size() && items[j][0] <= q) {
+                maxBea = max(maxBea, items[j][1]);
+                j++;
+            }
+            res[i] = maxBea;
+        }
+        return res;
+    }
+};
