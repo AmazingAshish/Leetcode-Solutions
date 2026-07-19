2012c6220c78e7acad3795c6ec7ba6c3114054a6
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:21:20 +0530

    Create 776-NAryTreePostorderTraversal

---
+++ 776-NAryTreePostorderTraversal/776-NAryTreePostorderTraversal.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/19/2026, 10:21:17 PM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorder(Node* root) {
+        if (!root) return ans;
+        for(auto& child: root->children)
+            postorder(child);
+        ans.push_back(root->val);
+        return ans;
+    }
+};
