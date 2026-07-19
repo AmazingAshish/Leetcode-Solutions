fa8164587994dea767b9bbb932efb917bb349ac9
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:22:45 +0530

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/19/2026, 10:22:43 PM
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        
+        queue<pair<TreeNode*, bool>> q;  // (node, is_left)
+        q.push({root, false});
+        int totalSum = 0;
+        
+        while (!q.empty()) {
+            auto [node, isLeft] = q.front();
+            q.pop();
+            
+            if (isLeft && !node->left && !node->right) {
+                totalSum += node->val;
+            }
+            
+            if (node->left) {
+                q.push({node->left, true});
+            }
+            if (node->right) {
+                q.push({node->right, false});
+            }
+        }
+        
+        return totalSum;
+    }
+};
