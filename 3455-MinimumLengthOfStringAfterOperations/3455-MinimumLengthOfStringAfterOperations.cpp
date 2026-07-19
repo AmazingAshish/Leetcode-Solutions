4cc030c2dadba7dcc12e00658db38ec35cd23b21
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:11:33 +0530

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/19/2026, 10:11:30 PM
+class Solution {
+
+public:
+
+    int minimumLength(string s) {
+
+        vector<int> charFrequency(26, 0);
+
+        int totalLength = 0;
+
+        for (char currentChar : s) {
+
+            charFrequency[currentChar - 'a']++;
+
+        }
+
+        for (int frequency : charFrequency) {
+
+            if (frequency == 0) continue;
+
+            if (frequency % 2 == 0) {
+
+                totalLength += 2;
+
+            } else {
+
+                totalLength += 1;
+
+            }
+
+        }
+
+        return totalLength;
+
+    }
+
+};
