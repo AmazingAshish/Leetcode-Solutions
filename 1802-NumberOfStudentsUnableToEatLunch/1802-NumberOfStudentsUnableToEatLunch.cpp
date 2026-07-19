9299017565c7725b9c0cb31396b14b4223b98dd9
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:17:12 +0530

    Create 1802-NumberOfStudentsUnableToEatLunch

---
+++ 1802-NumberOfStudentsUnableToEatLunch/1802-NumberOfStudentsUnableToEatLunch.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/19/2026, 10:17:10 PM
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int n=students.size();
+        queue<int> q;
+        for(int i=0;i<n;i++){
+           q.push(students[i]);
+        }
+        int count=0;
+        int i=0;
+        while(q.size()>0 && count!=q.size()){
+            if(q.front()==sandwiches[i]){
+                q.pop();
+                i++;
+                count=0;
+            }
+            else{
+                q.push(q.front());
+                q.pop();
+                count++;
+            }
+        }
+        return q.size();
+    } 
+};
