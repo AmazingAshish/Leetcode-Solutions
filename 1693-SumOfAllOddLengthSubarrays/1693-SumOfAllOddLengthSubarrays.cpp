f64c1ff72c89e79510f07b06e7768a950cd44e2c
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:17:44 +0530

    Create 1693-SumOfAllOddLengthSubarrays

---
+++ 1693-SumOfAllOddLengthSubarrays/1693-SumOfAllOddLengthSubarrays.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/19/2026, 10:17:42 PM
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& A) {
+        int res = 0, n = A.size();
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+        }
+        return res;
+    }
+};
