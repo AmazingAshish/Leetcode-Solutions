b3a821a16db610b74487210e7b8079ca198ac84b
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:18:58 +0530

    Create 1435-XorQueriesOfASubarray

---
+++ 1435-XorQueriesOfASubarray/1435-XorQueriesOfASubarray.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/19/2026, 10:18:56 PM
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int size = arr.size();
+        vector<int> prefXor(size);
+        
+        for(int indx = 0; indx < size; indx++) {
+            if(indx == 0) 
+                prefXor[indx] = arr[indx];
+            else 
+                prefXor[indx] = prefXor[indx - 1] ^ arr[indx];
+        }
+        
+        vector<int> res(queries.size());
+        int indx = 0;
+
+        for(const auto& quer : queries) {
+            int left = quer[0], right = quer[1];
+            res[indx++] = prefXor[right] ^ ((left == 0) ? 0 : prefXor[left - 1]);
+        }
+
+        return res;
+    }
+};
