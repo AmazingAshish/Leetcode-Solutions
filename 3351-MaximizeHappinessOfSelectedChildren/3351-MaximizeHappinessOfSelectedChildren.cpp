8ea091e23616fdb060e7cd37a52ff8bc424959fe
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:11:50 +0530

    Create 3351-MaximizeHappinessOfSelectedChildren

---
+++ 3351-MaximizeHappinessOfSelectedChildren/3351-MaximizeHappinessOfSelectedChildren.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/19/2026, 10:11:48 PM
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        //ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        sort(begin(happiness), end(happiness), greater<int>());
+        int i = 0;
+        long long res = 0;
+
+        while(k--) {
+            happiness[i] = max(happiness[i] - i, 0);
+            res += happiness[i++];
+        }
+
+        return res;
+    }
+};
