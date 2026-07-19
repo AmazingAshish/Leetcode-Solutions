71b6edd60d1e78bca9f1b79f402b9a7c308caf5f
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:11:34 +0530

    Create 3451-StringCompressionIii

---
+++ 3451-StringCompressionIii/3451-StringCompressionIii.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/19/2026, 10:11:32 PM
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int cnt = 1, n = word.size();
+        char ch = word[0];
+        for(int i=1;i<n;i++){
+            if(word[i] == ch && cnt < 9)cnt++;
+            else{
+                comp.push_back(cnt+'0');
+                comp.push_back(ch);
+                ch = word[i];
+                cnt = 1;
+            }
+        }
+        comp.push_back(cnt+'0');
+        comp.push_back(ch);
+        return comp;
+    }
+};
