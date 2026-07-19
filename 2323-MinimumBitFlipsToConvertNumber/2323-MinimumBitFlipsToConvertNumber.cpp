d41163306a1f6e90c49cc4e764f31def755b8070
Ashish Kumar <108587663+AmazingAshish@users.noreply.github.com>
2026-07-19 22:14:56 +0530

    Create 2323-MinimumBitFlipsToConvertNumber

---
+++ 2323-MinimumBitFlipsToConvertNumber/2323-MinimumBitFlipsToConvertNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/19/2026, 10:14:54 PM
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorResult = start ^ goal;  // XOR to find differing bits
+        int count = 0;
+        
+        // Count the number of 1's in the XOR result
+        while (xorResult > 0) {
+            count += xorResult & 1;  // Check if the least significant bit is 1
+            xorResult >>= 1;         // Right shift to check the next bit
+        }
+        
+        return count;
+    }
+};
